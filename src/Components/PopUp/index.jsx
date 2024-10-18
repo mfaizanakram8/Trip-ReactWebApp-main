e3a0105f3ca40c49e225bef1336894e97838c9e0
mfaizanakram8 <mfaizanakram536@gmail.com>
2024-10-18 13:56:00 +0500

    helloupdate2

--- src/Components/PopUp/index.jsx
+++ src/Components/PopUp/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Popup.css';
+import './popup.css';
 
 const Popup = ({ isVisible, onClose }) => {
   return (
--- src/pages/Welcome/index.jsx
+++ src/pages/Welcome/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import './Welcome.css'
 import { useNavigate } from 'react-router-dom';
 import LogoBack from '../../Components/LogoBack';
